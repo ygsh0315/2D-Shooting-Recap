e430c1571ab7f190b0fbb5ca091a23ce2d8207b4
Ellian <62099455+ygsh0315@users.noreply.github.com>
2023-01-04 20:39:37 +0900

    project setting

---
+++ 2D Shooting Recap/Assets/PlayerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 사용자 입력에 따라 상하좌우로 이동하고 싶다.
+// 필요 속성 : 이동 속도
+public class PlayerMove : MonoBehaviour
+{
+    // 필요 속성 : 이동 속도
+    float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 사용자 입력에 따라 상하좌우로 이동하고 싶다.
+        // 1. 사용자의 입력에 따라
+        float h = Input.GetAxis("Horizontal");
+        // 2. 방향이 필요
+        Vector3 dir = Vector3.right;
+        // 3. 이동하고 싶다.
+        // P = P0 + vt
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
